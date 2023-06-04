4faf98988b1f8d092dd81d222745c94e56ae6c97
Hao1122 <chinleehao@gmail.com>
2023-06-04 21:40:05 +0800

    Updated by Lee Hao
    Add two pages

--- client/src/Pages/Deposit.js
+++ client/src/Pages/Deposit.js
@@ -1,107 +1,46 @@
-import React, { useState } from "react";
-import { Formik, Form, Field, ErrorMessage } from "formik";
-import * as Yup from "yup";
+import React from "react";
 import Back from "../Components/back";
 import HistoryIcon from "@mui/icons-material/History";
-import Success from "../assets/tick.svg";
-import { Visibility, VisibilityOff } from "@mui/icons-material";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 
 export default function Deposit() {
-  const [showPassword, setShowPassword] = useState(false);
-  const togglePasswordVisibility = () => {
-    setShowPassword((prevState) => !prevState);
-  };
-  const initialValues = {
-    withdrawAmount: "",
-    walletAddress: "",
-    transactionPassword: "",
-  };
-
-  const validationSchema = Yup.object().shape({
-    withdrawAmount: Yup.number().required("Please Enter Withdraw Amount"),
-    walletAddress: Yup.string()
-      .min(10)
-      .max(10)
-      .required("Please Enter Bank Account Name"),
-    transactionPassword: Yup.string().required("Please Enter Password"),
-  });
   return (
     <div className="center margin">
       <div className="header">
         <div className="first-row white flex-row space-between">
           <Back />
-          <div className="big-title white">Withdraw</div>
+          <div className="big-title white">Deposit</div>
           <div className="icon">
             <HistoryIcon sx={{ fontSize: 30 }} />
           </div>
         </div>
       </div>
-
-      <div className="box margin">
-        <div className="big-title">Bonus Point Balance</div>
-        <div className="flex-row space-between border-bottom">
-          <div className="data">
-            <div className="dollar">5.0 USDT</div>
+      <div className="bigBox">
+        <div className="flex-row margin">
+          <div className="deposit">TRC20</div>
+          <div className="deposit">ERC20</div>
+          <div className="deposit">BEP20</div>
+        </div>
+        <div className="box">
+          <div className="big-title">Top Up USDT Wallet Address (TRC20)</div>
+          <div className="flex-row space-between border-bottom">
+            <div className="data">
+              <p>thesyhfuj764724hydaty782gyh</p>
+            </div>
+            <div className="data">
+              <span className="icon">
+                <ContentCopyIcon />
+              </span>
+            </div>
           </div>
-          <div className="data">
-            <span className="icon">
-              <img className="footer-image" src={Success} />
-            </span>
+        </div>
+
+        <div className="box">
+          <div className="data border-bottom">
+            <div className="text-align">TRC20</div>
           </div>
         </div>
       </div>
-
-      <Formik initialValues={initialValues} validationSchema={validationSchema}>
-        <Form className="formContainer">
-          <label className="label-field white">Withdraw Amount</label>
-          <div className="input-field space-between">
-            <Field
-              autoComplete="off"
-              type="number"
-              name="withdrawAmount"
-              placeholder="Withdraw Amount"
-            />
-          </div>
-          <label className="label-field white">Wallet Address</label>
-          <div className="input-field space-between">
-            <Field
-              autoComplete="off"
-              type="text"
-              name="walletAddress"
-              placeholder="Wallet Address"
-            />
-          </div>
-          <label className="label-field white">Transaction Password</label>
-          <div className="input-field space-between">
-            <Field
-              autoComplete="off"
-              type={showPassword ? "text" : "password"}
-              name="transactionPassword"
-              placeholder="Transaction Password"
-            />
-            <span
-              className="toggle-password"
-              onClick={togglePasswordVisibility}
-            >
-              {showPassword ? <VisibilityOff /> : <Visibility />}
-            </span>
-          </div>
-          <div className="header">
-            <div className="first-row">
-              <p className="white">Noted: -</p>
-              <p className="white">
-                Withdraw fee 5 USDT or 5 % whichever higher, Min. Withdaraw
-                Amount: 10
-              </p>
-            </div>
-          </div>
-          <div className="button">
-            <div className="sign-up create-account">
-              <p className="white">Submit</p>
-            </div>
-          </div>
-        </Form>
-      </Formik>
     </div>
   );
 }
--- client/src/Pages/Game.js
+++ client/src/Pages/Game.js
@@ -1,9 +1,151 @@
 import React from "react";
+import Back from "../Components/back";
+import HistoryIcon from "@mui/icons-material/History";
+import Bronze from "../assets/bronze.svg";
+import { Progress, Space } from "antd";
+import WhatsAppIcon from "../assets/whatsapp.png";
+import Footer from "../Components/Footer";
 
 export default function Game() {
+  const formatMoney = (percent) => {
+    const amount = percent * 100; // Assuming the percentage represents a fraction of a total amount
+    const formattedAmount = amount.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
+    return (
+      <div>
+        <p className="white">{formattedAmount}</p>
+        {/* <p>{percent * 100}%</p> */}
+      </div>
+    );
+  };
+
+  const formatDailyProfit = (percent) => {
+    const currencySymbol = "$";
+    const profit = percent * 1000; // Assuming the percentage represents daily profit
+    return (
+      <div>
+        <p className="white">
+          {currencySymbol}
+          {profit.toLocaleString("en-US")}
+        </p>
+        <p className="white small-font">Daily Profit </p>
+        {/* {formatMoney(percent)} */}
+      </div>
+    );
+  };
+
+  const formatAssetBalance = (percent) => {
+    const currencySymbol = "$";
+    const balance = percent * 100; // Assuming the percentage represents asset balance
+    return (
+      <div>
+        <p className="white">
+          {currencySymbol}
+          {balance.toLocaleString("en-US", "USD")}
+        </p>
+        <p className="white small-font">Asset Balance</p>
+        {/* {formatMoney(percent)} */}
+      </div>
+    );
+  };
   return (
     <div>
-      <div className="center">Game Page</div>
+      <div className="center">
+        <div className="header">
+          <div className="first-row white flex-row space-between">
+            <Back />
+            <div className="icon flex-row">
+              <p>Transaction History</p>
+              <HistoryIcon sx={{ fontSize: 30 }} />
+            </div>
+          </div>
+        </div>
+
+        <div className="content flex-row ">
+          <span className="padding">
+            <img
+              className="profile-image"
+              src={Bronze}
+              alt="User Profile Image"
+            />
+          </span>
+          <span className="content-text white padding">
+            <p>Testing 123</p>
+            <p>2023.01.01 - 00:00</p>
+          </span>
+        </div>
+        <Space wrap className="">
+          <div className="progress-container">
+            <Progress
+              type="circle"
+              percent={25}
+              format={() => formatMoney(8.88)}
+              className="custom-progress"
+              strokeWidth={10}
+              strokeColor={{
+                "0%": "#108ee9",
+                "100%": "#87d068",
+              }}
+              size={200}
+            />
+          </div>
+          <div className="flex-column space-between">
+            <div className="progress-container">
+              <Progress
+                type="circle"
+                percent={20}
+                status="exception"
+                format={() => formatDailyProfit(0.7)}
+                className="custom-progress"
+                strokeWidth={10}
+                strokeColor={{
+                  "0%": "#108ee9",
+                  "100%": "#87d068",
+                }}
+                size={100}
+              />
+            </div>
+            <div className="progress-container">
+              <Progress
+                type="circle"
+                percent={20}
+                format={() => formatAssetBalance(1)}
+                className="custom-progress"
+                strokeWidth={10}
+                strokeColor={{
+                  "0%": "#108ee9",
+                  "100%": "#87d068",
+                }}
+                size={100}
+              />
+            </div>
+          </div>
+        </Space>
+        <div className="button">
+          <div className="sign-up">Start</div>
+        </div>
+        <div className="data flex-end">
+          <div className="circle">
+            <img
+              className="footer-image"
+              src={WhatsAppIcon}
+              alt="Whatsapp Icon"
+            />
+          </div>
+        </div>
+        <div className="text-align">
+          <p className="white small-font">Important Hint</p>
+          <ul>
+            <li className="white small-font">Working hours: 9:30 - 23:00</li>
+            <li className="white small-font">
+              For inquiries about applicants, please consult the agency
+            </li>
+          </ul>
+        </div>
+        <Footer />
+      </div>
     </div>
   );
 }
